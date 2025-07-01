3b048daec916ff16b33bfabd8d2c341bf05e4f48
lordemmag-devops <lordemmag@yahoo.com>
2025-07-01 17:44:38 +0100

    Initial commit - Python Flask application.

---
+++ test_app.py
@@ -0,0 +1,19 @@
+
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() ad client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Python CI/CD Demo" in response.data
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.json == {"status": "healthy"}
