c534c883960807e7853c182af75ec0ea5c2588a7
lordemmag-devops <lordemmag@yahoo.com>
2025-07-02 14:43:30 +0100

    update pycache and test_pp

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from app import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    with app.test_client() ad client:
+    with app.test_client() as client:
         yield client
 
 def test_home(client):
